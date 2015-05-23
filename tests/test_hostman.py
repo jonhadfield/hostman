12e8e897b4d3a666ce7072a15c338bda99ad7698
Jon Hadfield <jon@lessknown.co.uk>
2015-05-23 16:18:12 +0100

    remove un-used test

--- tests/test_hostman.py
+++ tests/test_hostman.py
@@ -1,28 +1,19 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+import os
+print sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'hosts')))
+print sys.path
+import pytest
+from hosts import Hosts, HostsEntry, exception, utils
 
-"""
-test_hostman
-----------------------------------
-
-Tests for `hostman` module.
-"""
-
-import unittest
-
-from hostman import hostman
-
-
-class TestHostman(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_something(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+#def test_add_single_ipv4_host_by_detection(tmpdir):
+#    """
+#    Test the addition of an ipv4 host succeeds
+#    """
+#    hosts_file = tmpdir.mkdir("etc").join("hosts")
+#    hosts_file.write("127.0.0.1\tlocalhost\n")
+#    hosts = Hosts(path=hosts_file.strpath)
+#    new_entry = '3.4.5.6 bob jane.com'
+#    hosts.add(entry=new_entry, force=False)
+#    assert hosts.count(new_entry).get('address_matches') == 1
