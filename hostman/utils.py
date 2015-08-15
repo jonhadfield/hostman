72dd0a150c5a4fe87b8571fa94a14b82ffa653b9
Jon Hadfield <jon@lessknown.co.uk>
2015-08-15 13:53:50 +0100

    localise functions.

---
+++ hostman/utils.py
@@ -0,0 +1,22 @@
+import os
+
+
+def is_readable(path=None):
+    """
+    Test if the supplied filesystem path can be read
+    :param path: A filesystem path
+    :return: True if the path is a file that can be read. Otherwise, False
+    """
+    if os.path.isfile(path) and os.access(path, os.R_OK):
+        return True
+    return False
+
+
+def is_writeable(path=None):
+    """
+    Test if the supplied filesystem path can be written to
+    :param path: A filesystem path
+    :return: True if the path is a file that can be written. Otherwise, False
+    """
+    if os.path.isfile(path) and os.access(path, os.W_OK):
+        return True
